8315e1ed12b6388074a192d3a10a9e2530d10ee4
asrith2208 <uudoonasrith@gmail.com>
2026-02-24 23:50:16 +0530

    fix:HEADER NEW UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Routes, Route } from 'react-router-dom';
 import MainLayout from './components/layout/MainLayout';
+import ScrollToTop from './components/ScrollToTop';
 import Home from './pages/Home';
 import Products from './pages/Products';
 import Checkout from './pages/Checkout';
@@ -10,17 +11,20 @@ import News from './pages/News';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<MainLayout />}>
-        <Route index element={<Home />} />
-        <Route path="products" element={<Products />} />
-        <Route path="checkout" element={<Checkout />} />
-        <Route path="tracking" element={<Tracking />} />
-        <Route path="locations" element={<Locations />} />
-        <Route path="awards" element={<Awards />} />
-        <Route path="news" element={<News />} />
-      </Route>
-    </Routes>
+    <>
+      <ScrollToTop />
+      <Routes>
+        <Route path="/" element={<MainLayout />}>
+          <Route index element={<Home />} />
+          <Route path="products" element={<Products />} />
+          <Route path="checkout" element={<Checkout />} />
+          <Route path="tracking" element={<Tracking />} />
+          <Route path="locations" element={<Locations />} />
+          <Route path="awards" element={<Awards />} />
+          <Route path="news" element={<News />} />
+        </Route>
+      </Routes>
+    </>
   );
 }
 
